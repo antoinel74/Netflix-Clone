8a4544ad4feb4a205cf72886cbe61b89f3b3e3d2
antoinel74 <antoine.lansman@gmail.com>
2023-11-27 16:59:18 +0100

    overscroll x + add placement for rating, still improvements to make

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,36 +32,37 @@ export default function Home() {
   return (
     <div className="w-full">
       <h1 className="text-4xl mb-8">Trending Movies</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 mx-6 cursor-pointer">
-        {movies.map((movie: Movie) => (
-          <div
-            key={movie.id}
-            className="rounded-sm overflow-hidden shadow-lg bg-dark flex flex-col"
-          >
-            <img
-              className="h-96 w-full object-cover"
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <div className="p-2">
-              <h2 className="text-xl font-semibold mb-2 h-35 my-3">
-                {movie.title}
-              </h2>
-              <p className="text-gray-600 mb-4 line-clamp-3">
-                {movie.overview}
-              </p>
-              <div>
-                <p className="text-gray-700">Popularity: {movie.popularity}</p>
-                <p className="text-gray-700">
-                  Vote Average: {movie.vote_average}
-                </p>
-                <p className="text-gray-700">
-                  Release Date: {convertDateFormat(movie.release_date)}
-                </p>
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-5 overflow-x-auto w-full hide-scrollbar">
+        <div className="">
+          <div className="flex whitespace-nowrap">
+            {movies.map((movie: Movie) => (
+              <div
+                key={movie.id}
+                className="relative rounded-xl overflow-hidden bg-dark flex flex-col mr-5"
+                style={{ minWidth: "250px" }}
+              >
+                <div className="relative">
+                  <img
+                    className="h-50 w-auto object-contains object-center rounded-xl"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <div className="absolute top-2 right-2 bg-black bg-opacity-50 text-white p-1 rounded">
+                    {movie.vote_average.toFixed(2)}
+                  </div>
+                </div>
+                <div className="p-2">
+                  <h2 className="text-sm font-semibold h-4 my-2">
+                    {movie.title}
+                  </h2>
+                </div>
+                <div className="text-sm flex justify-between p-2">
+                  <span>{convertDateFormat(movie.release_date)}</span>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
--- src/utils/convertDate.tsx
+++ src/utils/convertDate.tsx
@@ -1,5 +1,6 @@
 export function convertDateFormat(dateString: string): string {
   const parts = dateString.split("-");
-  const newDate = `${parts[2]}/${parts[1]}/${parts[0]}`;
+  /*   const newDate = `${parts[2]}/${parts[1]}/${parts[0]}`; */
+  const newDate = `${parts[0]}`;
   return newDate;
 }
